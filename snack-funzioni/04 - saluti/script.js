27a4f5fe0896c1890320973757854c626a473c25
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-06 16:40:23 +0100

    ended 04-saluti

--- snack-funzioni/04 - saluti/script.js
+++ snack-funzioni/04 - saluti/script.js
@@ -4,10 +4,12 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function printHello(personName) {
+    console.log(`Ciao`, personName);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+printHello(name);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
